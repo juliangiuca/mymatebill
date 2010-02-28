b52ed847488f509feecaec61b5bec3c6c927d9d2
juliangiuca <julian.giuca@gmail.com>
2010-02-28 23:02:06 +1100

    make calendar actually work and start on line item support

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,8 @@ class EventsController < ApplicationController
     account = current_user.accounts.find(params[:event][:account_id])
     actor = current_user.actors.find_or_create_by_name(params[:actor])
     @event = account.events.create!(params["event"].merge(:actor_id => actor.id))
-    redirect_to :action => :show, :id => @event.id
+    #redirect_to :action => :show, :id => @event.id
+    redirect_to :action => :index
 
   rescue ActiveRecord::RecordNotFound
     flash[:error] = "Owie! That wasn't your account!"
@@ -33,7 +34,7 @@ class EventsController < ApplicationController
     if @actors.present?
       render :text => @actors.map{|x| x.name}.to_json
     else
-      render :nothing => true
+      render :text => "".to_json
     end
 
   end
