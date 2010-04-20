d6b6aec80295e4bce214749857ec24aaca966f5f
Mini Foot <ffmini@MiniFoot.local>
2010-04-21 09:57:56 +1000

    using the same exception notification as Kaylee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,9 @@ config.action_view.cache_template_loading            = true
 
 require 'smtp-tls'
 
-ExceptionNotifier.exception_recipients = %w(kaylee_support@hoolio.net)
-ExceptionNotifier.sender_address = %(error@hoolio.net)
-ExceptionNotifier.email_prefix = "[Kaylee - Error] "
+ExceptionNotifier.exception_recipients = "inara_support@hoolio.net"
+ExceptionNotifier.sender_address = "error@hoolio.net"
+ExceptionNotifier.email_prefix = "[Inara - Error] "
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
