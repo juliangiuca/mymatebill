990383025640440241aaba0d5a0456778c5c4a7c
local <local@kaylee.borkt.com>
2010-04-20 23:26:29 +1000

    treetop as a plugin

--- config/environment.rb
+++ config/environment.rb
@@ -19,9 +19,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-    config.gem "factory_girl", :source => "http://gemcutter.org"
-    config.gem 'treetop', :version => ">= 1.4.5"
-    config.gem 'polyglot', :version => ">= 0.3.1"
 
 
   # Only load the plugins named here, in the order given (default is alphabetical).
