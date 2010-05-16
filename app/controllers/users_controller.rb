b1314e4c02875021551ba094ea793b658a47535a
juliangiuca <julian.giuca@gmail.com>
2010-05-16 15:37:09 +1000

    remove activate code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,34 +63,7 @@ class UsersController < ApplicationController
     @user.destroy
     redirect_to users_path
   end
-
-  def forgot
-    if request.post?
-      user = User.find_by_email(params[:user][:email])
-      if user
-        user.create_reset_code
-        flash[:notice] = "Reset code sent to #{user.email}"
-      else
-        flash[:notice] = "#{params[:user][:email]} does not exist in system"
-      end
-      redirect_back_or_default('/')
-    end
-  end
-
- def reset
-    @user = User.find_by_reset_code(params[:reset_code]) unless params[:reset_code].nil?
-    if request.post?
-      if @user.update_attributes(:password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
-        self.current_user = @user
-        @user.delete_reset_code
-        flash[:notice] = "Password reset successfully for #{@user.email}"
-        redirect_back_or_default('/')
-      else
-        render :action => :reset
-      end
-    end
-  end
-
+  
   # There's no page here to update or destroy a user.  If you add those, be
   # smart -- make sure you check that the visitor is authorized to do so, that they
   # supply their old password along with a new one to update it, etc.
