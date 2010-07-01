112767c9d8c7a5c10ecb08799682c7cb649553b8
juliangiuca <julian.giuca@gmail.com>
2010-07-01 22:27:14 +1000

    make sure the features check a transaction when a sub transaction changes state

--- features/step_definitions/sub_transaction_steps.rb
+++ features/step_definitions/sub_transaction_steps.rb
@@ -9,11 +9,16 @@ Then /^The transaction is automatically marked as unpaid and balances set$/ do
   @transaction.steps.map{|x| x.state}.count("paid").should == 3
   @biller.cash_in = 975
 
-  friends = @identity.associates.select{|x| x != @biller}
-  number_of_friends = friends.length + 1
+  friends = @transaction.steps.map{|x| x.from}
+  number_of_friends = friends.length
   friends.each do |friend|
-    friend.outgoing_transactions.should have(1).records
-    friend.outgoing_transactions.first.amount.should == @amount / number_of_friends
+      friend.outgoing_transactions.should have(1).records
+      friend.outgoing_transactions.first.amount.should == @amount / number_of_friends
+    if friend.outgoing_transactions.first.state == "paid"
+      friend.cash_out.should == (@amount / number_of_friends) * -1
+    else
+      friend.cash_out.should == 0
+    end
   end
 end
 
