16b2b81181621d36de5aa5759e8204e11abb2252
juliangiuca <julian.giuca@gmail.com>
2010-07-21 23:27:10 +1000

    add some comments to the shamble of the unicorn config script

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -32,16 +32,8 @@ end
 
 
 before_fork do |server, worker|
-  ##
-  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
-  # immediately start loading up a new version of itself (loaded with a new
-  # version of our app). When this new Unicorn is completely loaded
-  # it will begin spawning workers. The first worker spawned will check to
-  # see if an .oldbin pidfile exists. If so, this means we've just booted up
-  # a new Unicorn and need to tell the old one that it can now die. To do so
-  # we send it a QUIT.
-  #
-  # Using this method we get 0 downtime deploys.
+
+  # Kick down to user www for the master. this makes deployment not need sudo
   begin
     uid, gid = Process.euid, Process.egid
     user, group = 'www', 'www'
@@ -61,6 +53,16 @@ before_fork do |server, worker|
     end
   end
 
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
   old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
